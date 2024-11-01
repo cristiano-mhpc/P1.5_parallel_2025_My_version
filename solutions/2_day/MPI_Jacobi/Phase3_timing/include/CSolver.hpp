cf89e25602de11c9cb6eaee8f2c4ab8e669927b6
christian_hpc <ticachristian20@gmail.com>
2024-11-02 06:35:08 +0800

    added MPI_Jacobi

---
+++ solutions/2_day/MPI_Jacobi/Phase3_timing/include/CSolver.hpp
@@ -0,0 +1,62 @@
+#ifndef CSolver_HPP
+#define CSolver_HPP
+
+#include <sstream>
+#include "CMesh.hpp"
+#include <fstream>
+#include <iostream>
+#include <vector>
+#include <string>
+#include <iomanip>
+#include <mpi.h>
+#include "Parallel_CSimple_timer.hpp"
+
+template <typename T>
+class CSolver {
+public:
+  void jacobi(CMesh<T> &M, int my_rank, int comm_sz, const size_t &max_steps,
+              const size_t &PrintInterval, size_t local_rows, int rem, MPI_Comm comm){
+    size_t step{0};
+    size_t i, j;
+    size_t n = M.rows;
+    size_t m = M.colm;
+
+    while (step < max_steps) {
+      /**
+       * each process performs TWO MPI_Send_recv()s one with the PREVIOUS process
+       * and another with the NEXT process.
+      */
+      int next_rank = (my_rank == comm_sz - 1) ? MPI_PROC_NULL : my_rank + 1;
+      int prev_rank = (my_rank == 0) ? MPI_PROC_NULL : my_rank - 1;
+      {
+        CSimple_timer t("communication",  my_rank, comm_sz, MPI_COMM_WORLD);
+        //send to prev_rank, recv from next_rank
+        MPI_Sendrecv(&M.field[m + 1], m-2, MPI_DOUBLE, prev_rank, 1,
+                    &M.field[(n-1)*m + 1], m-2, MPI_DOUBLE, next_rank, 1, comm, MPI_STATUS_IGNORE);
+      
+        //send to next_rank, recv from prev_rank
+        MPI_Sendrecv(&M.field[(n-2)*(m-1) + 1], m-2, MPI_DOUBLE, next_rank, 0,
+                     &M.field[1], m - 2, MPI_DOUBLE, prev_rank, 0, comm, MPI_STATUS_IGNORE);
+
+      }
+
+      {
+        CSimple_timer t("execution", my_rank, comm_sz, MPI_COMM_WORLD);
+        for (i = 1; i < n - 1 ; i++) {
+          for (j = 1; j < m - 1; j++) {
+            M.new_field[i * m + j] =
+                0.25*(M.field[(i + 1) * m + j] + M.field[(i - 1) * m + j] +
+                        M.field[i * m + j + 1] + M.field[i * m + j - 1]);
+         }
+        }
+      }
+      
+      M.new_field.swap(M.field);
+
+      step++;
+      
+    }//while
+  }//jacobi
+};//CSolver
+
+#endif
