b99cd4e85cd84f42a6331668a6098ba26e7f1e53
christian_hpc <ticachristian20@gmail.com>
2024-11-11 04:19:39 +0800

    working_run_with_plotting

--- fftw/D2-exercise2/provided_code/C/utilities.h
+++ fftw/D2-exercise2/provided_code/C/utilities.h
@@ -44,8 +44,8 @@ typedef struct {
 double seconds();
 inline int index_f ( int i1, int i2, int i3, int n1, int n2, int n3 );
 
-void plot_data_1d( char* name, int n1, int n2, int n3, int dir, double* data );
-void plot_data_2d( char* name, int n1, int n2, int n3, int dir, double* data );
+void plot_data_1d( char* name, int n1, int n2, int n3, int n1_local, int  n1_local_offset, int dir, double* data );
+void plot_data_2d( char* name, int n1, int n2, int n3, int n1_local, int  n1_local_offset, int dir, double* data);
 void init_fftw( fftw_dist_handler* fft, int n1, int n2, int n3, MPI_Comm comm );
 void close_fftw( fftw_dist_handler* fft );
 
